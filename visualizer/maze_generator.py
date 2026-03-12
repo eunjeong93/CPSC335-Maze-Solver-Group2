047c2e3a18ee951b0d675b27357c98a21bd1877c
David Le <davidle173@gmail.com>
2026-03-11 20:15:24 -0700

    main path generation test
    - Main Path Generation Function implemented
    - Path generation tested with 5% and 10% total area coverage with a maximum 25% coverage.
    - Main path generation is recommended to be set at 5% for significantly lower generation times due to a larger range.

--- visualizer/maze_generator.py
+++ visualizer/maze_generator.py
@@ -25,6 +25,9 @@ class Maze:
     Maze data also contains a path array,
     which stores the valid path in the generated maze.
     '''
+    def __init__(self, main_path):
+        pass
+
 
 
 
@@ -36,16 +39,30 @@ def generate_random_maze(size_x, size_y):
 
     :param size_x: The x length of the maze; 7 <= x <= 300
     :param size_y: The y length of the maze; 7 <= x <= 300
-    :returns: A maze object containing generated maze
+    :returns: A Maze object containing generated maze
             data and maze size; Returns None if x or y
             length is invalid
     '''
     min_size = 7
-    max_size = 300
+    max_size = 100
 
     if size_x > max_size or size_y > max_size: return None
 
     if size_x <= min_size or size_y <= min_size: return None
+
+    maximum_maze_area = size_x * size_y
+
+    start_pos = 0
+    end_pos = 0
+    while start_pos == end_pos:
+        start_pos = (random.randint(1, size_x - 1), random.randint(1, size_y - 1))
+        end_pos = (random.randint(1, size_x - 1), random.randint(1, size_y - 1))
+
+    main_path = []
+    min_path_coverage = maximum_maze_area * 0.1
+    max_path_coverage = maximum_maze_area * 0.25
+    while len(main_path) < min_path_coverage or len(main_path) > max_path_coverage:
+        main_path = generate_path(size_x, size_y, start_pos, end_pos)
 
     """
     Maze Generation Algorithm
@@ -54,29 +71,28 @@ def generate_random_maze(size_x, size_y):
     Declare two random points as the start and end of the maze
     While the two random points are invalid:
         Pick two random points
-        If the two points are not on the same point and not directly adjacent to each other:
+        If the two points are not on the same point:
             End the loop
     
     While a valid path does not exist:
         Generate a random path
-        If the path has >= 5% and < 30% coverage of the space:
+        If the path has >= 5% and < 25% coverage of the space (including start and end):
             End the loop
     
     Store valid path
 
-    While the amount of generated walls is not valid:
-        Generate walls around the generated path
-        If the amount of walls have <= 40% of path coverage:
-            End the loop
-
-    Generate random access paths that connect to only the start or end
-    based on the size of the maze space
+    While total path coverage is < 70% of the entire space
+        Generate random access paths that connect to only the start or end
+        based on the size of the maze space
 
     Fill in remaining space with walls and return Maze object
     """
+    print(main_path)
     max_maze_x = size_x - 1
     max_maze_y = size_y - 1
     # Border generation
+
+    return main_path
 
 def generate_path(size_x, size_y, start_pos, end_pos):
     '''
@@ -90,61 +106,6 @@ def generate_path(size_x, size_y, start_pos, end_pos):
     :returns path_arr: An array of 2-tuples that contains the path data
     '''
 
-    """
-    Path Generation Algorithm
-
-    Start from the start position
-    While current point is not equal to the end position
-        Choose a random adjacent point that is in the same row or column but not both
-        
-        If the chosen point is adjacent to more than 1 point:
-            End the current iteration
-        Otherwise:
-            Set the current position to the chosen point 
-    """
-    """
-    A path CANNOT have 4+ path cells directly adjacent to each other.
-
-    W = Wall
-    C = Current
-    N = Next
-    O = Not Allowed
-
-    Yes Example         No Example      Yes Example
-
-    W N O               C C             C C C C
-    W C N               C C                 C
-    W N O
-
-    Cell Position Map
-    [x - 1, y + 1][x + 0, y + 1][x + 1, y + 1]
-    [x - 1, y + 0][x + 0, y + 0][x + 1, y + 0]
-    [x - 1, y - 1][x + 0, y - 1][x + 1, y - 1]
-
-    Current Position = (x, y)
-    Adjacent Possible Positions:
-        Corner Blocked
-            - (Left-Top):       (x + 1, y), (x, y - 1)
-            - (Left-Bottom):    (x, y + 1), (x + 1, y)
-            - (Right-Top):      (x - 1, y), (x, y - 1)
-            - (Right-Bottom):   (x - 1, y), (x, y + 1)
-        Single Blocked
-            - (Left):           (x, y), (x + 1, y), (x, y - 1)
-            - (Right):          (x, y), (x - 1, y), (x, y - 1)
-            - (Top):            (x - 1, y), (x + 1, y), (x, y - 1)
-            - (Bottom):         (x, y + 1), (x - 1, y), (x + 1, y)
-    Adjacent Not Possible Positions
-        Corner Blocked
-            - (Left-Top):       (x - 1, y), (x, y + 1)
-            - (Left-Bottom):    (x - 1, y), (x, y - 1)
-            - (Right-Top):      (x, y + 1), (x + 1, y)
-            - (Right-Bottom):   (x + 1, y), (x, y - 1)
-        Single Blocked
-            - (Left):           (x - 1, y)
-            - (Right):          (x + 1, y)
-            - (Top):            (x, y + 1)
-            - (Bottom):         (x, y - 1)
-    """
     path_arr = [] # Store path data in an array of 2-tuples
     current_pos = start_pos # Intialize current position to start (x, y)
     path_arr.append(current_pos) # Include the start position into the path
@@ -157,49 +118,90 @@ def generate_path(size_x, size_y, start_pos, end_pos):
         top = (current_pos[0], current_pos[1] + 1)
         bottom = (current_pos[0], current_pos[1] - 1)
 
-        print("Current Posit:\t" + str(current_pos))
-        print("Adjacent Left:\t" + str(left))
-        print("Adjacent Righ:\t" + str(right))
-        print("Adjacent Top:\t" + str(top))
-        print("Adjacent Bott:\t" + str(bottom))
-
         # Check each cell if it already exists in the path array
-        # Also check if the path is single wall adjacent
-        if left not in path_arr or left[0] > 1: next_positions.append(left)
+        # Additionally check for maze bounds
+        if left not in path_arr and left[0] > 1: next_positions.append(left)
 
-        if right not in path_arr or right[0] < size_x: next_positions.append(right)
+        if right not in path_arr and right[0] < size_x: next_positions.append(right)
 
-        if top not in path_arr or top[1] < size_y: next_positions.append(top)
+        if top not in path_arr and top[1] < size_y: next_positions.append(top)
 
-        if bottom not in path_arr or bottom[1] > 1: next_positions.append(bottom)
+        if bottom not in path_arr and bottom[1] > 1: next_positions.append(bottom)
 
-        # Check for single adjacent positions
-        # Look ahead by one position in order to check for directly adjacent possible positions
-        new_next = []
-        for next_pos in next_positions:
-            next_left = (next_pos[0] - 1, next_pos[1])
-            next_right = (next_pos[0] + 1, next_pos[1])
-            next_top = (next_pos[0], next_pos[1] + 1)
-            next_bot = (next_pos[0], next_pos[1] - 1)
-
-            if next_left or next_right or next_top or next_bot not in path_arr:
-                new_next.append(next_pos)
+        if len(next_positions) == 0: break
 
         # Randomly pick between the remaining next positions
-        current_pos = random.choice(new_next)
+        current_pos = random.choice(next_positions)
+
         path_arr.append(current_pos)
-        print("New Position:\t" + str(current_pos))
-        print()
 
         # Clear next positions for the next iteration
         next_positions.clear()
     
     return path_arr
 
-if __name__ == '__main__':
-    arr = generate_path(7, 7, (1, 3), (4, 5))
+def generate_single_access_path(size_x, size_y, main_path, *single_path):
+    '''
+    Description: Randomly picks either the start or end position and
+    generates an access path that only connects to one of them. The main_path
+    parameter's purpose is to prevent the generation from overlapping too much
+    with the main generated path, so this function should be called after the
+    main path has been generated.
 
-    for coord in arr: print(coord)
+    :param size_x: The x length of the maze
+    :param size_y: The y length of the maze
+    :param main_path: The main generated solution path of the maze
+                    that connects the start and ending positions.
+    :param *single_path: Any number of single paths can be passed as a parameter
+                    to prevent overlapping of multiple single access paths.
+    :returns: A single access path that connects to either the start or end position.
+    '''
+
+def generate_walls_and_borders(size_x, size_y, main_path, *single_path):
+    '''
+    Description: Generates the walls and border of the maze, where the border
+    is 2 units larger than the provided x and y lengths. The walls will be filled
+    into the remaining spaces that are non-path cells.
+
+    :param size_x: The x length of the maze
+    :param size_y: The y length of the maze
+    :param main_path: The main generated solution path of the maze that
+                    connects the start and ending positions.
+    :param *single_path: Any number of single paths can be passed as a parameter
+                    to help determine the remaining spaces in the maze.
+    :returns: An array of 2-tuples containing the locations of each wall and the border.
+    '''
+
+def export_maze_data_to_text_file(maze):
+    '''
+    Description: Exports maze data into a text file. The text file will contain
+    the translated version of the maze into its correct symbols for searching
+    algorithm usage.
+
+    :param maze: A Maze object containing complete maze data.
+    :returns: A boolean True if the export was successful; False otherwise
+    '''
+if __name__ == '__main__':
+    sizex = 100
+    sizey = 100
+    arr = generate_random_maze(sizex, sizey)
+    path_coverage = (len(arr) / (sizex * sizey)) * 100
+
+    start = arr[0]
+    end = arr[len(arr) - 1]
+
+    print("Main Path Coverage: " + str(path_coverage) + "%")
+    for i in range(sizex):
+        for j in range(sizey):
+            if (i, j) == start:
+                print("S", end=" ")
+            elif (i, j) == end:
+                print("E", end=" ")
+            elif (i, j) in arr:
+                print(".", end=" ")
+            else:
+                print("#", end=" ")
+        print()
 
 
     
